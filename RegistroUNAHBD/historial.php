183e3865f93ba9cdeeace31b9286da2bdd21ba0b
Alejandra Rodríguez <a_rodriguez0605@hotmail.com>
2018-05-01 13:24:53 -0600

    Add files via upload

---
+++ RegistroUNAHBD/historial.php
@@ -0,0 +1,213 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Historial Académico - UNAH</title>
+    <link rel="icon" href="img/imgunah/logo.png" sizes="24x24" type="image/svg">
+
+
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css"> 
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="css/util (2).css">
+  <link rel="stylesheet" type="text/css" href="css/main (2).css">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <script src="js/misFunciones.js"></script>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+
+        <a class="navbar-brand" href="index.php"><img src="img/imgunah/logo.png">Universidad Nacional Autónoma de Honduras</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Página Principal</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="about.php">Planes de Estudio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="registro.php">Registrarse</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Ingresar
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="loginestudiante.php">Estudiante</a>
+                <a class="dropdown-item" href="logincatedratico.php">Catedrático</a>
+        <!--        <a class="dropdown-item" href="portfolio-3-col.html"></a>
+                <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a> -->
+              </div>
+           <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Descargar
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="#">Manual de usuario</a>
+              </div>
+
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+<body>
+<br><br>
+
+    <div class="container-table100">
+
+          <span class="login100-form-title">
+            Historial Académico
+          </span>  
+      <div class="wrap-table100">
+        <div class="table100 ver1 m-b-110">
+          <div class="table100-head">
+            <table>
+              <thead>
+                <tr class="row100 head">
+                  <th class="cell100 column1">Asignatura</th>
+                  <th class="cell100 column2">Código</th>
+                  <th class="cell100 column4">Catedrático</th>
+                  <th class="cell100 column5">Periodo</th>
+                  <th class="cell100 column5">Nota</th>
+                  <th class="cell100 column3">Observación</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+
+          <div class="table100-body js-pscroll">
+            <table>
+              <tbody>
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+                <tr class="row100 body">
+                  <td class="cell100 column1"></td>
+                  <td class="cell100 column2"></td>
+                  <td class="cell100 column3"></td>
+                  <td class="cell100 column4"></td>
+                  <td class="cell100 column5"></td>
+                </tr>
+
+ 
+              </tbody>
+            </table>
+          </div>
+       </div> </div></div>
+
+
+      <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Base de Datos I &copy; Proyecto final 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/tilt/tilt.jquery.min.js"></script>
+  
+  <script >
+    $('.js-tilt').tilt({
+      scale: 1.1
+    })
+  </script>
+  <script src="js/main (2).js"></script>
+
+</body>
+</html>
